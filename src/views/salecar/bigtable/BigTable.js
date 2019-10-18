ec0bc8dfdbcfb71beb25948d77af15d14ae3e8c2
xuxingxing <973553289@qq.com>
2019-10-18 20:01:26 +0800

    2.从本地获取数据，同时创建用于columns映射的字典

--- src/views/salecar/bigtable/BigTable.js
+++ src/views/salecar/bigtable/BigTable.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Table } from 'antd';
 
 export default class BigTable extends Component {
     render () {
+        //试着从本地存储中读取column字段
+        let columnsFromLocalStore = localStorage.getItem('columns');
+        console.log(columnsFromLocalStore);
+        //如果这个字段读取出来时null,表示用户第一次来本网站或者清空缓存
+        if(columnsFromLocalStore === null){
+            //第一次进入，给用户本地赋予一个值
+            localStorage.setItem('columns', JSON.stringify(['image', 'id', 'brand', 'series', 'color']));
+        }
+        //再次从本地存储列表存储信息，并转换
+        const columns = JSON.parse(localStorage.getItem('columns'));
         return (
             <div>
-                123456
+                <Table
+                    columns={[{'title':'商标', 'key':'brand', 'dataIndex':'brand'}]}
+                />
             </div>
         );
     }
---
+++ src/views/salecar/bigtable/columnsMap.js
@@ -0,0 +1,24 @@
+//暴露一个json，作为table的columns映射的字典
+export default {
+    'id':{
+        'title':'编号'
+    },
+    'brand':{
+        'title':'商标'
+    },
+    'series':{
+        'title': '车系'
+    },
+    'color':{
+        'title': '颜色'
+    },
+    'engine':{
+        'title': '发动机'
+    },
+    'exhaust':{
+        'title': '尾气'
+    },
+    'fuel':{
+        'title': '燃料' 
+    }
+}
